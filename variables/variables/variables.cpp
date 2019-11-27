be811c38628f9c4a73ce0718af589c0dfa4b1108
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2019-11-27 17:53:28 +0200

    section 1

---
+++ variables/variables/variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+using namespace std;
+
+void main()
+{
+	int numberCats = 5;	
+	cout << "Hello World:" << flush ;
+	cin >> numberCats;
+	string Text = "lol";
+	string OtherText = "kek";
+	cout << Text + "lol" + OtherText << endl;
+	cout << "max int:" << INT_MAX << endl;
+	cout << "size of Text:" << sizeof(Text) << endl;
+
+
+
+
+
+
+}
